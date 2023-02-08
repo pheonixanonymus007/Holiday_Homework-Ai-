fb426d1daefa178ee556a77db1b48092d4e2190c
pheonixanonymus007 <96486609+pheonixanonymus007@users.noreply.github.com>
2023-02-08 12:55:04 +0530

    Add files via upload

---
+++ Practice(Achyut).py
@@ -0,0 +1,43 @@
+# %%
+#answer1
+num = float(input("Input a number: "))
+if num > 0:
+   print("It is positive number")
+elif num == 0:
+   print("It is Zero")
+else:
+   print("It is a negative number")
+
+# %%
+#answer2
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in Kilogram: "))
+print("Your body mass index is: ",(weight / (height * height), 2))
+
+# %%
+#answer3
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+if a == b:
+  print ("Both inputs are equal")
+else:
+  print ("Your input is not equal.")
+
+# %%
+
+#answer4
+num = int (input ("Enter any number to test whether it is odd or even:" ))
+
+if (num % 2) == 0:
+
+ print(" The number is even ")
+
+else:
+
+ print ("The provided number is odd")
+
+##Practice by Achyut Anand
+
+
+
